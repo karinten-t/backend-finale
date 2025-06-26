4ba775cea90d264a5348704a6124910021c2fe9d
Pascal <pascalrintareseketeti@gmail.com>
2025-06-26 22:38:06 +0300

    nivce

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, 
      origins=[
-         "http://localhost:3000", 
+         
          "https://frontend-testfinal.onrender.com"
      ],
      supports_credentials=True)
