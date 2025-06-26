054a2df193f128fe917d448fd3f1ea8a114fe261
Pascal <pascalrintareseketeti@gmail.com>
2025-06-26 22:34:04 +0300

    few changes

--- app.py
+++ app.py
@@ -7,8 +7,16 @@ from sqlalchemy.exc import IntegrityError
 from config import db
 from models import User, Recipe
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, 
+     origins=[
+         "http://localhost:3000", 
+         "https://frontend-testfinal.onrender.com"
+     ],
+     supports_credentials=True)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'super-secret-key'
