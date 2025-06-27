c09f7ac632f49b393c106c82307d3df45f040654
Pascal <pascalrintareseketeti@gmail.com>
2025-06-27 04:16:50 +0300

    syntax edit

--- app.py
+++ app.py
@@ -148,4 +148,4 @@ def delete_recipe(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create tables if they don't exist
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
